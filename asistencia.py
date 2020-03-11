37dd3dfae992bb40ab31454bde0eda6c7d6b75d3
jesteban94 <jestebanza94@gmail.com>
2020-03-11 14:49:12 -0500

    primer codigo

--- asistencia.py
+++ asistencia.py
@@ -1,25 +1,31 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
+
 
 class asistencia(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.mis_vent = self.driver.window_handles[0]
+
 
     def test_navegador(self):
         navegador = self.driver
         navegador.get("https://www.ucundinamarca.edu.co/")
+        navegador.execute_script("window.open('');")
+
         time.sleep(20)
-        buplataforma = navegador.find_element_by_xpath("//*[@id='sp-header']/div/div/div/div[2]/div[2]/a")
+        buplataforma = navegador.find_element_by_xpath("//*[@id='sppb-addon-1565022666579']/div/div/div/a[3]")
+        time.sleep(5)
         buplataforma.click()
-        time.sleep(3)
 
-
-
-
-
+        time.sleep(5)
+        usuario = navegador.find_element_by_id("usuario")
+        usuario.send_Keys("jezamora")
+        time.sleep(10)
 
 
 if __name__ == "__main__":
---
+++ prueba.py
@@ -0,0 +1,58 @@
+# import the libs
+from selenium import webdriver
+from time import sleep
+
+# create the initial window
+driver = webdriver.Chrome()
+
+# go to the home page
+driver.get('https://www.zomato.com')
+sleep(10)
+
+# storing the current window handle to get back to dashbord
+main_page = driver.current_window_handle
+
+# wait for page to load completely
+sleep(5)
+
+# click on the sign in tab
+driver.find_element_by_xpath('//*[@id ="signin-link"]').click()
+
+sleep(5)
+
+# click to log in using facebook
+driver.find_element_by_xpath('//*[@id ="facebook-login-global"]/span').click()
+
+# changing the handles to access login page
+for handle in driver.window_handles:
+    if handle != main_page:
+        login_page = handle
+
+    # change the control to signin page
+driver.switch_to.window(login_page)
+
+# user input for email and password
+print('Enter email id : ', end='')
+email = input().strip()
+print('Enter password : ', end='')
+password = input().strip()
+
+# enter the email
+driver.find_element_by_xpath('//*[@id ="email"]').send_keys(email)
+
+# enter the password
+driver.find_element_by_xpath('//*[@id ="pass"]').send_keys(password)
+
+# click the login button
+driver.find_element_by_xpath('//*[@id ="u_0_0"]').click()
+
+# change control to main page
+driver.switch_to.window(main_page)
+
+sleep(10)
+# print user name
+name = driver.find_element_by_xpath('/html/body/div[4]/div/div[1]/header/div[2]/div/div/div/div/span').text
+print('Your user name is : {}'.format(name))
+
+# closing the window
+driver.quit()
